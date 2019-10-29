0ffde9125771995da79b8bbcdf87e05a964b3d24
Gianluca Governi <gianlucagoverni@gmail.com>
2019-10-29 19:47:06 +0100

    edit index

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => {
             <h2>This is the website of the the Tai Pan boat.</h2>
 
             <p>
-                Need a boat? <Link to="/contact">contact me!</Link>
+                Need a boat or a skipper? <Link to="/contact">contact me!</Link>
             </p>
         </Layout>
     )
