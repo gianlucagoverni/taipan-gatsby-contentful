cf78c5fda71923dd712298480e24ca315cfcd288
Gianluca Governi <gianlucagoverni@gmail.com>
2019-11-19 21:15:19 +0100

    init footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {graphql, useStaticQuery} from 'gatsby'
 
 import footerStyles from './footer.module.scss'
+import ContactIcon from './contactIcon'
+import Logo from './logo'
 
 const Footer = () => {
     const data = useStaticQuery(graphql`
@@ -15,7 +17,12 @@ const Footer = () => {
     `)
     return (
         <footer className={footerStyles.footerContainer}>
-            <p>Createdy by {data.site.siteMetadata.author}, 2019</p>
+            {/* <p>Createdy by {data.site.siteMetadata.author}, 2019</p> */}
+            <div className={footerStyles.logoWrapper}>
+                <ContactIcon />
+                <Logo />
+                <div><a href="https://goo.gl/maps/PGbBdZnkdHNrg6kB6" className={footerStyles.coordinate} target="_blank" rel="noopener noreferrer">+42° 4’ 35.1’’, +10° 18’ 55.53’</a></div>
+            </div>
         </footer>
     )
 }
