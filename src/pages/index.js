6e71dc3df1243cfe065750f1f13e1ebf49463912
Gianluca Governi <gianlucagoverni@gmail.com>
2019-11-24 20:19:36 +0100

    incliude about component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ import React from "react"
 import Layout from '../components/layout'
 import Head from '../components/head'
 import HeroSlider from '../components/heroSlider'
-import  MoreInfo from '../components/moreInfo'
+import About from '../components/aboutSection'
+import MoreInfo from '../components/moreInfo'
 import indexStyles from './index.module.scss'
 // import HeroImage from '../components/heroImage'
 // import hero from '../assets/hero-image.jpg'
@@ -16,10 +17,7 @@ const IndexPage = () => {
             <div className={indexStyles.heroSliderWrapper}>
                 <HeroSlider />
             </div>
-            <h1>Hello.</h1>
-            <h2>This is the website of the the Tai Pan boat.</h2>
-            
-           
+            <About />
            <MoreInfo />
         </Layout>
     )
